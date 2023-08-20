d7201cd9f710dd153fd6dcd900a4d570a38c775b
grapefruit13 <liam.heo11@gmail.com>
2023-08-21 04:10:04 +0900

    함수 매개변수 오탈자 수정

--- js/greetings.js
+++ js/greetings.js
@@ -5,20 +5,21 @@ const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
+  console.log(loginInput.value);
   paintGreetings(username);
 }
 
-function paintGreetings(username) {
+function paintGreetings(savedUsername) {
   greeting.innerText = `Welcome ${savedUsername}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
-const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
